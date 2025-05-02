6423cf8038e2cdb5a2bd82aec22f5a0829b91bce
jmhogan <jmhogan@fnal.gov>
2025-05-02 16:11:26 -0500

    update xsecs, add doublet plot

--- combineLimits/PlotLimits.py
+++ combineLimits/PlotLimits.py
@@ -230,7 +230,7 @@ def PlotLimits(limitDir,limitFile,tempKey):
     else: expected95.GetYaxis().SetRangeUser(.0005+.00001,20.1)
     expected95.GetXaxis().SetRangeUser(800,2000)
     expected95.GetXaxis().SetTitle(signal+" mass [GeV]")
-    expected95.GetYaxis().SetTitle("#sigma (Bbj #rightarrow tWbj) [pb]")
+    expected95.GetYaxis().SetTitle("#sigma (pp #rightarrow bqB) [pb]")
     expected95.GetYaxis().SetTitleOffset(1.05)
 
     expected68.Draw("3same")
