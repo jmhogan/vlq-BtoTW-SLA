4bdab1624e9494d2f0d400c971e3807abda8fb86
jmhogan <jmhogan@fnal.gov>
2025-07-24 13:49:13 -0500

    multiply xsec by BR

--- samples.py
+++ samples.py
@@ -20,7 +20,7 @@ for syst in uncorrList_sf:
         systListFullPlots.append(syst+year)
 
 
-systListABCDnn = ['val','train','pNetTtag','pNetWtag']#'param0','param1','param2','param3','param4','param5','param6','param7','lastbin']
+systListABCDnn = ['correct','train','pNetTtag','pNetWtag']#'param0','param1','param2','param3','param4','param5','param6','param7','lastbin']
 
 # out of date
 factorABCDnn = {'allWlep':'0.034045477',
@@ -53,6 +53,9 @@ class sample:
 # We will use these for plotting in this "xsec" folder, but keep them as 1 in the actual sample definitions for ROOT files
 # From Xanda, for "singlet" B prod with a b quark, for 1% width, for 50% tW
 xsec = {'800':0.1187124, '900':0.0640113, '1000':0.0362987, '1100':0.0215009, '1200':0.0131348, '1300':0.0082629, '1400':0.0053213, '1500':0.0035078, '1600':0.0022829, '1700':0.0014947, '1800':0.0009898, '1900':0.0006519, '2000':0.0004499}
+for mass in xsec.keys():
+    xsec[mass] = xsec[mass]*0.5  # these xsec values need to be multiplied by the 0.5 BR explicitly
+    
 # extrapolating from ln(xsec) fit I get 0.000238 for 2200
 
 Bprime_M1000_2016APV = sample("Bprime_M1000_2016APV", 1.0, "2016APV", "Bprime_M1000_2016APVULNanoList.txt", "/BprimeBtoTW_M-1000_NWALO_TuneCP5_13TeV-madgraph-pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM")
